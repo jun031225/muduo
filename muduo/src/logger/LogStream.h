2e87206171da11698760e9b52511937d3a05e2b7
LJ <92453498+jun031225@users.noreply.github.com>
2024-03-26 12:46:28 +0000

    add

--- muduo/src/logger/LogStream.h
+++ muduo/src/logger/LogStream.h
@@ -2,7 +2,7 @@
 
 #include "FixedBuffer.h"
 
-// GeneralTemplate:用于LogStream<<时间，sourceFIle等
+// 输出通用模版，用于LogStream<<时间，sourceFIle等
 class GeneralTemplate : noncopyable
 {
 public:
@@ -24,27 +24,27 @@ public:
         buffer_.append(data, len);
     }
 
-    const SmallBuffer &buffer() const { return buffer_; } // 获取buffer
+    const SmallBuffer& buffer() const { return buffer_; } // 获取buffer
     void resetBuffer() { buffer_.reset(); }
 
     // 重载运算符"<<"，以便可以像cout那样 << 任意类型
-    LogStream &operator<<(short);
-    LogStream &operator<<(unsigned short);
-    LogStream &operator<<(int);
-    LogStream &operator<<(unsigned int);
-    LogStream &operator<<(long);
-    LogStream &operator<<(unsigned long);
-    LogStream &operator<<(long long);
-    LogStream &operator<<(unsigned long long);
-    LogStream &operator<<(float v);
-    LogStream &operator<<(double v);
-    LogStream &operator<<(char c);
-    LogStream &operator<<(const void *p);
-    LogStream &operator<<(const char *str);
-    LogStream &operator<<(const unsigned char *str);
-    LogStream &operator<<(const std::string &str);
-    LogStream &operator<<(const SmallBuffer &buf);
-    LogStream &operator<<(const GeneralTemplate &g); // 相当于(const char*, int)的重载
+    LogStream& operator<<(short);
+    LogStream& operator<<(unsigned short);
+    LogStream& operator<<(int);
+    LogStream& operator<<(unsigned int);
+    LogStream& operator<<(long);
+    LogStream& operator<<(unsigned long);
+    LogStream& operator<<(long long);
+    LogStream& operator<<(unsigned long long);
+    LogStream& operator<<(float v);
+    LogStream& operator<<(double v);
+    LogStream& operator<<(char c);
+    LogStream& operator<<(const void *p);
+    LogStream& operator<<(const char *str);
+    LogStream& operator<<(const unsigned char *str);
+    LogStream& operator<<(const std::string &str);
+    LogStream& operator<<(const SmallBuffer &buf);
+    LogStream& operator<<(const GeneralTemplate &g); // 相当于(const char*, int)的重载
 private:
     static const int maxNumericSize = 48; // 数字（整数或者浮点数）转换为字符串后最大的长度
     SmallBuffer buffer_;                  // 4KB，重载的所有运算符都是在向buffer中写数据，
